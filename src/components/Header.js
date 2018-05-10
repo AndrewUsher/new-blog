e9701fb24a94fff21bfcd85ee1cc7b022d4e3eb1
Andrew Usher <andrewusher00@gmail.com>
2018-05-10 16:55:31 -0500

    Add site branch

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import '../styles/_header.styl'
-
-const Header = () => (
-  <AppBar className="header" style={{ padding: '0 25px' }} showMenuIconButton={false} title="Casual Cognitions" zDepth={2} />
-)
-
-export default Header
--- src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from 'typography'
-
-const customTypography = new Typography({
-  baseFontSize: '22px',
-  baseLineHeight: 1.8
-})
-
-export default customTypography
