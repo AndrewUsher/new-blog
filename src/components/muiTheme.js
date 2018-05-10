2b9a29b42e7551954506a706645790ebca662333
Andrew Usher <andrewusher00@gmail.com>
2018-05-10 14:53:25 -0500

    Add material-ui theme config

---
+++ src/components/muiTheme.js
@@ -0,0 +1,14 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#3F51B5',
+    primary2Color: '#303F9F',
+    textColor: '#212121'
+  },
+  appBar: {
+    height: 70
+  }
+})
+
+export default muiTheme
