edea300bb772d9d18e09d8449a54d04bc0ab39e2
Andrew Usher <andrewusher00@gmail.com>
2018-05-10 16:35:36 -0500

    Complete bio component and styles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import profilePic from './profile-pic.png'
+import Paper from 'material-ui/Paper'
+import '../styles/_bio.styl'
 
-const Bio = () => (
-  <p>
+const Bio = ({ newDepth }) => (
+  <Paper className="bio" zDepth={newDepth || 1}>
     <img
       src={profilePic}
       alt={`Andrew Usher`}
     />
-    I'm <strong>Andrew Usher</strong> and I live and work in Memphis having fun with front-end!{' '}
-    <a href="https://twitter.com/andrewusher17">
-      You can follow me on Twitter as well!
-    </a>
-  </p>
+    <p>
+      I'm <strong>Andrew Usher</strong> and I live and work in Memphis having fun with front-end!{' '}
+      <a href="https://twitter.com/andrewusher17">
+        You can follow me on Twitter as well!
+      </a>
+    </p>
+  </Paper>
 )
 
 export default Bio
