81747d6ffcebe663ceae1c7682a6e9d67e3dd6d5
Andrew Usher <andrewusher00@gmail.com>
2018-05-11 15:42:06 -0500

    Modify Header componet for material-ui update Also use typography inside header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
 import '../styles/_header.styl'
 
-const Header = () => (
-  <AppBar className="header" style={{ padding: '0 25px' }} showMenuIconButton={false} title="Casual Cognitions" zDepth={2} />
+const styles = {
+  root: {
+    marginBottom: '40px',
+    padding: '10px 0 10px 0'
+  }
+}
+
+const Header = ({ classes }) => (
+  <AppBar position="static" color="primary" className={classes.root}>
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        Casual Cognitions
+      </Typography>
+    </Toolbar>
+  </AppBar >
 )
 
-export default Header
+export default withStyles(styles)(Header)
