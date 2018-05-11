3434ab1b982b47c64300776b2adfc40ae16a9831
Andrew Usher <andrewusher00@gmail.com>
2018-05-11 15:28:29 -0500

    Switch to createMuiTheme for latest version

--- src/utils/muiTheme.js
+++ src/utils/muiTheme.js
@@ -1,10 +1,10 @@
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { createMuiTheme } from 'material-ui/styles'
 
-const muiTheme = getMuiTheme({
+const muiTheme = createMuiTheme({
   palette: {
     primary1Color: '#3F51B5',
     primary2Color: '#303F9F',
-    textColor: '#212121'
+    textColor: '#111'
   },
   appBar: {
     height: 70
