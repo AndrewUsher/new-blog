9147f4c1156473e60594789312ce2715ce749b45
Andrew Usher <andrewusher00@gmail.com>
2018-05-11 15:42:27 -0500

    Update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
-import FlatButton from 'material-ui/FlatButton'
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card'
+import Card, { CardActions, CardHeader, CardContent } from 'material-ui/Card'
 import Paper from 'material-ui/Paper'
+import Button from 'material-ui/Button'
+import CssBaseline from 'material-ui/CssBaseline'
+import { withStyles } from 'material-ui/styles'
 import '../styles/_home.styl'
 
 import Bio from '../components/Bio'
@@ -13,30 +15,31 @@ class BlogIndex extends React.Component {
   render () {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
+    const { classes } = this.props
 
     return (
-      <div>
+      <div className={classes.root}>
         <Helmet title={siteTitle} />
+        <CssBaseline />
         <Bio />
         {posts.map(({ node }) => {
           const title = get(node, 'frontmatter.title') || node.fields.slug
           return (
-            <Paper zDepth={1} className="post">
-              <Card key={node.fields.slug}>
-                <CardHeader
-                  title={title}
-                  subtitle={node.frontmatter.date}
-                />
-                <CardText>
-                  {node.excerpt}
-                </CardText>
-                <CardActions>
-                  <Link to={node.fields.slug}>
-                    <FlatButton label="Read More" />
-                  </Link>
-                </CardActions>
-              </Card>
-            </Paper>
+            <Card key={node.fields.slug} className="post" raised>
+              <CardHeader
+                title={title}
+                subheader={node.frontmatter.date}
+              />
+
+              <CardContent>
+                {node.excerpt}
+              </CardContent>
+              <CardActions>
+                <Link to={node.fields.slug} style={{ textDecoration: 'none' }}>
+                  <Button className={classes.button}>Read More</Button>
+                </Link>
+              </CardActions>
+            </Card>
           )
         })}
       </div>
@@ -44,28 +47,28 @@ class BlogIndex extends React.Component {
   }
 }
 
-export default BlogIndex
+export default withStyles()(BlogIndex)
 
 export const pageQuery = graphql`
   query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
+          site {
+        siteMetadata {
+          title
+        }
+        }
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC }) {
+          edges {
         node {
           excerpt
           fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "DD MMMM, YYYY")
-            title
-          }
+          slug
         }
+        frontmatter {
+          date(formatString: "DD MMMM, YYYY")
+        title
       }
     }
   }
+}
+}
 `
